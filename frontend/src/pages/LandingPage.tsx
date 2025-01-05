71d13e41179ef154168e2e5e44d78a9e78abf2d9
abhishekbhonde <abhishekbhonde567@gmail.com>
2025-01-05 14:14:08 +0530

    feat: final commit of the project

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,10 +1,9 @@
 import { HeroHighlightDemo } from '../components/HeroHighlightDemo'
-import { FloatingNavDemo } from '../components/Navbar'
 
 const LandingPage = () => {
   return (
     <div className='h-screen w-screen '>
-            <FloatingNavDemo/>
+           
             <HeroHighlightDemo/>
     </div>
   )
